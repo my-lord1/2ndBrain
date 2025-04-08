223d5009a37d81f7d246c29bea55f1e12ca0fbab
Nivas Reddy <bn123@snu.edu.in>
2025-04-08 18:14:00 +0530

    created Browser Routes in fe

--- 2ndBrianFE/src/App.tsx
+++ 2ndBrianFE/src/App.tsx
@@ -1,11 +1,18 @@
-
+import { Signin }  from "./pages/signin"
+import { Signup } from "./pages/signup"
 import { Dashboard } from "./pages/dashboard"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App(){
   return (
-    <div>
-      <Dashboard/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="signin" element={<Signin />} />
+      <Route path="dashboard" element={<Dashboard/> } />
+    </Routes>
+    <Routes/>
+    </BrowserRouter>
 )}
 
 export default App
---
+++ 2ndBrianFE/src/pages/signin.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../components/Button";
+import {Input } from "../components/Input";
+
+export function Signin() {
+    return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+    <div className="bg-white rounded-xl border min-w-48 p-8">
+        <Input placeholder="Username" />
+        <Input placeholder="Password" />
+        <div className="flex justify-center pt-4">
+            <Button onClick={Signin} loading={false} variant="primary" text="Signin" fullWidth={true} startIcon={<span>🔑</span>} />
+        </div>
+    </div>
+</div>
+}
